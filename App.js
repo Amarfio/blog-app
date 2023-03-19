c58aa733b8c7f61705230a8a1eb36e3305f867ee
= <=>
2023-03-19 08:07:58 +0000

    first push for the application

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexPage from './pages/IndexPage';
+import BlogPage from './pages/BlogPage';
+import { View, Image, StyleSheet, Text  } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Index" component={IndexPage} options={{headerShown:false}}/>
+        <Stack.Screen name="BlogPage" component={BlogPage} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+// import { StatusBar } from 'expo-status-bar';
 
 const styles = StyleSheet.create({
   container: {
